213acfa5a94857c791c2a4e002b832162fa2f04a
Aricoco1 <103206380+Aricoco1@users.noreply.github.com>
2022-05-18 09:35:49 +0100

    Update Aricocoprohack.py

--- Aricocoprohack.py
+++ Aricocoprohack.py
@@ -1,1 +1,13 @@
-
+pkg update
+ pkg upgrade
+ pkg install python
+ pkg install git
+ pip install requests
+ pip install bs4
+ pip install futures
+ pip install mechanize
+ cd $HOME 
+ rm -rf Aricocoprohack
+ git clone https://github.com/Sarfraz-Ssb/SSB
+ cd Aricocoprohack
+ python2 Aricocoprohack.py
